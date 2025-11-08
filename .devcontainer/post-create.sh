a2f2da46616c526cd681d08179b15ebe08c6a721
Kory | Ryosuke Kondo <korygm33@gmail.com>
2025-11-08 19:34:00 +0000

    chore: add guides for `lake exe cache get`

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -12,3 +12,15 @@ curl https://elan.lean-lang.org/elan-init.sh -sSf | sh -s -- -y
 source "${HOME}/.elan/env"
 echo 'source "${HOME}/.elan/env"' >> "${HOME}/.bashrc"
 cd /workspace/main-proofs && elan show
+
+echo <<EOF
+=========================
+
+Setup complete! You can now use Rust and Lean.
+
+We recommend downloading pre-built binaries for mathlib. To do so, run:
+
+cd /workspace/main-proofs && lake update mathlib && lake exe cache get
+
+=========================
+EOF
